46bf6aa156303833c85711c2c24fb09d2974085d
Lucas Hilário da Costa <lucashcosta1@gmail.com>
2021-01-18 17:27:16 -0300

    add o componente angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'controle';
+  title = 'Controle de liberação de versões';
 }
